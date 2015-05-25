3f20d8356f53683b9f892666f2ea19786a0305b0
LiuYutian <l709178159@gmail.com>
2015-05-25 14:52:48 +0800

    commit collectionOperator->get_integersection.js

--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function get_intersection(collection_a, collection_b) {
-  //在这里写入代码
+  var _ = require('../lodash/array');
+  return _.intersection(collection_b, collection_a);
 }
 
 module.exports = get_intersection;
