b57f671f7f7a614f226cab9310edbcf04869d9cd
LiuYutian <l709178159@gmail.com>
2015-05-25 20:44:03 +0800

    commit filter->choose_even.js

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -1,11 +1,11 @@
 'use strict';
 
 function collect_all_even(collection) {
-  var _ = require('../lodash/array');
+    var _ = require('../lodash/array');
 
-  return _.remove(collection, function(n) {
+    return _.remove(collection, function(n) {
       return n % 2 === 0;
-  });
+    });
 }
 
 module.exports = collect_all_even;
--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,8 +1,11 @@
 'use strict';
 
 function choose_even(collection) {
+    var _ = require('../lodash/array');
 
-  //在这里写入代码
+    return _.remove(collection, function(n) {
+        return n % 2 === 0;
+    });
 }
 
 module.exports = choose_even;
