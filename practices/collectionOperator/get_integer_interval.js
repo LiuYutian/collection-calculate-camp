b85b86f59da242ccb5c853d0cdcd9457a62813d1
LiuYutian <l709178159@gmail.com>
2015-05-25 14:13:02 +0800

    commit collection

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -1,8 +1,23 @@
 'use strict';
 
 function get_integer_interval(number_a, number_b) {
-  //在这里写入代码
+    var array = [];
+
+    if(number_a < number_b) {
+        for(var i = number_a; i <= number_b; i++) {
+            array.push(i);
+        }
+        return array;
+    }
+
+    if(number_a > number_b) {
+        for(var i = number_a; i >= number_b; i--){
+            array.push(i);
+        }
+        return array;
+    }
+
+    return [number_a];
 }
 
 module.exports = get_integer_interval;
-
