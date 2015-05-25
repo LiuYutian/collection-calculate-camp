6e9a04a8d8004aa92f00144d051ebc3f886521d3
LiuYutian <l709178159@gmail.com>
2015-05-25 14:30:30 +0800

    commit collectionOperator->get_integer_interval_2.js

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -3,11 +3,9 @@
 function collect_all_even(collection) {
   var _ = require('../lodash/array');
 
-  collection = _.remove(collection, function(n) {
+  return _.remove(collection, function(n) {
       return n % 2 === 0;
   });
-
-  return collection;
 }
 
 module.exports = collect_all_even;
--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -1,7 +1,31 @@
 'use strict';
 
 function get_integer_interval_2(number_a, number_b) {
-  //在这里写入代码
+  var _ = require('../lodash/array');
+
+  return _.remove((get_integer_interval(number_a, number_b)), function(n) {
+     return n % 2 === 0;
+  });
+}
+
+function get_integer_interval(number_a, number_b){
+    var array = [];
+
+    if(number_a < number_b) {
+        for(var i = number_a; i <= number_b; i++) {
+            array.push(i);
+        }
+        return array;
+    }
+
+    if(number_a > number_b) {
+        for(var i = number_a; i >= number_b; i--){
+            array.push(i);
+        }
+        return array;
+    }
+
+    return [number_a];
 }
 
 module.exports = get_integer_interval_2;
