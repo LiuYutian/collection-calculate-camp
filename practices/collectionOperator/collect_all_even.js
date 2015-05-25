3824ef2a891115370d451a3ef223c796da4abf30
LiuYutian <l709178159@gmail.com>
2015-05-25 11:34:45 +0800

    commit collectionOperator->collect_all_even.js

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -2,6 +2,13 @@
 
 function collect_all_even(collection) {
   //在这里写入代码
+  var _ = require('../lodash/array');
+
+  collection = _.remove(collection, function(n) {
+      return n % 2 === 0;
+  });
+
+  return collection;
 }
 
 module.exports = collect_all_even;
