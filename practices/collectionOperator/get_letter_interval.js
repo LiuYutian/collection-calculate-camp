ee88211dfb8d2a43530ab872efbadd2c7311e99d
LiuYutian <l709178159@gmail.com>
2015-05-25 15:21:25 +0800

    commit collectionOperator->get_letter_interval.js

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -1,7 +1,23 @@
 'use strict';
 
 function get_letter_interval(number_a, number_b) {
-  //在这里写入代码
+    var array = [];
+
+    if(number_a < number_b) {
+        for(var i = number_a; i <= number_b; i++) {
+            array.push(String.fromCharCode(i+96));
+        }
+        return array;
+    }
+
+    if(number_a > number_b) {
+        for(var i = number_a; i >= number_b; i--){
+            array.push(String.fromCharCode(i+96));
+        }
+        return array;
+    }
+
+    return [String.fromCharCode(number_a+96)];
 }
 
 module.exports = get_letter_interval;
