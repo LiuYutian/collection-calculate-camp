57105d8b29ea262e61f48c8ab9f819eeed98a89c
LiuYutian <l709178159@gmail.com>
2015-05-26 12:50:52 +0800

    commit map->map_to_even.js

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,5 +1,12 @@
 'use strict';
 function map_to_even(collection){
-  return [2,4,6,8,10];
+  var _ = require('../lodash/collection');
+
+  return _.map(collection, timesTwo);
 }
+
+function timesTwo(n) {
+    return n * 2;
+}
+
 module.exports = map_to_even;
