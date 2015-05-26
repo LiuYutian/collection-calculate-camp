4ad6397715ad51ae905d6ac8f02a27a8078f298e
LiuYutian <l709178159@gmail.com>
2015-05-26 17:43:33 +0800

    change my collectionOperator->get_intersection.js

--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -1,8 +1,20 @@
 'use strict';
 
 function get_intersection(collection_a, collection_b) {
-  var _ = require('../lodash/array');
-  return _.intersection(collection_b, collection_a);
+    // var _ = require('../lodash/array');
+    // return _.intersection(collection_b, collection_a);
+
+    var array = [];
+
+    for(var i = 0; i < collection_b.length; i++) {
+        for(var j = 0; j <collection_a.length; j++) {
+            if(collection_a[j] === collection_b[i]) {
+                array.push(collection_a[j]);
+            }
+        }
+    }
+
+    return array;
 }
 
 module.exports = get_intersection;
