479c5d1d6cecbe634bfe463f812c238dbd11e44b
LiuYutian <l709178159@gmail.com>
2015-05-26 19:38:36 +0800

    change my collectionOperator->get_union.js

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -1,9 +1,30 @@
 'use strict';
 
 function get_union(collection_a, collection_b) {
-  var _  = require('../lodash/array');
+    // var _  = require('../lodash/array');
+    //
+    // return _.union(collection_a, collection_b);
 
-  return _.union(collection_a, collection_b);
+    for(var i = 0; i < collection_b.length; i++) {
+        collection_a.push(collection_b[i]);
+    }
+
+    var array = [];
+
+    for(var i = 0; i < collection_a.length; i++){
+        var exist = false;
+        for(var j = 0; j < array.length; j++) {
+            if(collection_a[i] == array[j]) {
+                exist = true;
+                break;
+            }
+        }
+        if(!exist) {
+            array.push(collection_a[i]);
+        }
+    }
+
+    return array;
 }
 
 module.exports = get_union;
