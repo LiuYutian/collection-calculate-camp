3d85ac5df1c6d27084b4dab1a1ff2ac870a3539f
LiuYutian <l709178159@gmail.com>
2015-05-26 13:01:57 +0800

    commit map->map_to_four_multiples_add_one.js

--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,6 +1,11 @@
 'use strict';
 var map_to_four_multiples_add_one = function(collection){
-  return [5,9,13,17,21];
+    var _ = require('../lodash/collection');
+  return _.map(collection, timesFour);
 };
+
+function timesFour(n) {
+    return n*4+1;
+}
 
 module.exports = map_to_four_multiples_add_one;
