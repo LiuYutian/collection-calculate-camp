7b8e0d638de9ec9977bb5ef91b2846b7c089ced4
LiuYutian <l709178159@gmail.com>
2015-05-26 17:21:57 +0800

    change my collectionOperator->collect_last_element.js

--- practices/collectionOperator/collect_last_element.js
+++ practices/collectionOperator/collect_last_element.js
@@ -1,9 +1,11 @@
 'use strict';
 
 function collect_last_element(collection) {
-    var _ = require('../lodash/array');
+    // var _ = require('../lodash/array');
+    //
+    // return _.last(collection);
 
-    return _.last(collection);
+    return collection[collection.length-1];
 }
 
 module.exports = collect_last_element;
