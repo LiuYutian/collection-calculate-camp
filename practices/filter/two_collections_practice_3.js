bba8e5bafc69d826186612db7eb87cf1f49d1566
LiuYutian <l709178159@gmail.com>
2015-05-26 11:26:35 +0800

    commit filter->two_collections_practice_3.js

--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -1,8 +1,17 @@
 'use strict';
 
 function choose_divisible_integer(collection_a, collection_b) {
+    var _ = require('../lodash/lodash');
 
-  //在这里写入代码
+    var array = [];
+
+    for(var i = 0; i < collection_b; i++) {
+        array.push(_.findLast(collction_a, function(n) {
+            return n % collection_b[i] == 0;
+        }))
+    }
+
+    return array;
 }
 
 module.exports = choose_divisible_integer;
