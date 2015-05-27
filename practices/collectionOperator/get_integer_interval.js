b119a97c62e9e60788bbad30674e74befd5b7606
LiuYutian <l709178159@gmail.com>
2015-05-27 10:01:30 +0800

    change function collectionOperator->get_integer_interval.js

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -4,20 +4,34 @@ function get_integer_interval(number_a, number_b) {
     var array = [];
 
     if(number_a < number_b) {
-        for(var i = number_a; i <= number_b; i++) {
-            array.push(i);
-        }
-        return array;
+        return aGreaterb(number_a, number_b);
     }
 
     if(number_a > number_b) {
-        for(var i = number_a; i >= number_b; i--){
-            array.push(i);
-        }
-        return array;
+        return aLessThanb(number_a, number_b);
     }
 
     return [number_a];
+}
+
+function aGreaterb(number_a, number_b) {
+    var array = [];
+
+    for(var i = number_a; i <= number_b; i++) {
+        array.push(i);
+    }
+
+    return array;
+}
+
+function aLessThanb(number_a, number_b) {
+    var array = [];
+
+    for(var i = number_a; i >= number_b; i--){
+        array.push(i);
+    }
+
+    return array;
 }
 
 module.exports = get_integer_interval;
