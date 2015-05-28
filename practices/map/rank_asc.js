9dcb537a533b65bc7d72f505226559f61076a009
LiuYutian <l709178159@gmail.com>
2015-05-28 16:37:36 +0800

    change no map->rank_asc.js

--- practices/map/rank_asc.js
+++ practices/map/rank_asc.js
@@ -1,9 +1,19 @@
 'use strict';
 var rank_asc = function(collection){
-    return collection.sort(sortNumber);
-};
+    if(collection.length === 0 || collection.length === 1) {
+        return collection;
+    }
 
-function sortNumber(a,b) {
-    return b - a;
+    for(var i = 0; i < collection.length; i++) {
+        for(var j = i; j < collection.length; j++) {
+            if(collection[i] < collection[j]) {
+                collection[i] = collection[i] + collection[j];
+                collection[j] = collection[i] - collection[j];
+                collection[i] = collection[i] - collection[j];
+            }
+        }
+    }
+
+    return collection;
 }
 module.exports = rank_asc;
