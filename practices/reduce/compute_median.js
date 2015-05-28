99ffd9f3a15694e0e4d76c681124ac43f5972564
LiuYutian <l709178159@gmail.com>
2015-05-28 19:49:30 +0800

    commit no lodash reduce->compute_median.js

--- practices/reduce/compute_median.js
+++ practices/reduce/compute_median.js
@@ -1,9 +1,21 @@
 'use strict';
 
 function compute_median(collection) {
-  //在这里写入代码
+    for(var i = 0; i < collection.length; i++) {
+        for(var j = i; j < collection.length; j++) {
+            if(collection[i] > collection[j]) {
+                collection[i] = collection[i] + collection[j];
+                collection[j] = collection[i] - collection[j];
+                collection[i] = collection[i] - collection[j];
+            }
+        }
+    }
+
+    if(collection.length % 2 === 0) {
+        return (collection[collection.length/2-1]+collection[collection.length/2])/2;
+    }else{
+        return collection[parseInt(collection.length/2)];
+    }
 }
 
 module.exports = compute_median;
-
-
