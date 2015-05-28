8e8a68aaad155da23e7e919dec26783b994b2469
LiuYutian <l709178159@gmail.com>
2015-05-28 15:51:27 +0800

    change no lodash map->number_map_to_word_over_26.js

--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,8 +1,12 @@
 'use strict';
 var number_map_to_word_over_26 = function(collection){
-    var _ = require('../lodash/collection');
+    var array = [];
 
-    return _.map(collection, MapChar);
+    for(var i = 0; i < collection.length; i++) {
+        array[array.length] = MapChar(collection[i]);
+    }
+
+    return array;
 };
 
 function MapChar(n) {
