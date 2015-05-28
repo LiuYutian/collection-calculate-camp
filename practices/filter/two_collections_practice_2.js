db060fd86a55ec0e41d90f09e0786393a37db931
LiuYutian <l709178159@gmail.com>
2015-05-28 09:16:25 +0800

    change no lodash filter->two_collections_practice_2.js

--- practices/filter/two_collections_practice_2.js
+++ practices/filter/two_collections_practice_2.js
@@ -1,9 +1,26 @@
 'use strict';
 
 function choose_no_common_elements(collection_a, collection_b) {
-    var _ = require("../lodash/array");
+    var result = {};
+    var array = [];
 
-    return _.xor(collection_a, collection_b);
+    for(var i = 0; i <collection_b.length;i++) {
+        collection_a.push(collection_b[i]);
+    }
+
+    for(var i=0; i<collection_a.length; i++) {
+        result[[collection_a[i]]] = result[[collection_a[i]]]||0;
+        result[collection_a[i]]++;
+    }
+
+    for(var key in result) {
+        if(result[key] === 1) {
+            array[array.length] = key;
+        }
+    }
+
+    return array;
 }
 
 module.exports = choose_no_common_elements;
+
