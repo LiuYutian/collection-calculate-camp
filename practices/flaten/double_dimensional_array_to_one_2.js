35834c66de69ff750e82770baa1d451c67c48d37
LiuYutian <l709178159@gmail.com>
2015-05-28 14:02:57 +0800

    change no lodash flaten->double_dimensional_array_to_one_2.js

--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -1,9 +1,36 @@
 'use strict';
 
 function double_to_one(collection) {
-    var _ = require('../lodash/array');
+    var arrayUnidimensional = [];
+    var result = {};
+    var array = []
 
-    return _.union(_.flatten(collection));
+    for(var i = 0; i < collection.length; i++) {
+        if(collection[i].length === undefined){
+            arrayUnidimensional[arrayUnidimensional.length] = collection[i];
+        }
+
+        for(var j = 0; collection[i].length!=undefined && j < collection[i].length ; j++) {
+            arrayUnidimensional[arrayUnidimensional.length] = collection[i][j];
+        }
+
+    }
+
+    for(var i = 0; i < arrayUnidimensional.length; i++) {
+        var exist = false;
+
+        for(var j = 0; j < array.length; j++) {
+            if(arrayUnidimensional[i] === array[j]) {
+                exist = true;
+                break;
+            }
+        }
+        if(!exist) {
+            array[array.length] = arrayUnidimensional[i];
+        }
+    }
+
+    return array;
 }
 
 module.exports = double_to_one;
