a3f7e980b382d8a2e54d43f5a1f1f8ce6cf31af1
LiuYutian <l709178159@gmail.com>
2015-05-28 16:39:12 +0800

    change no map->rank_dese.js

--- practices/map/rank_desc.js
+++ practices/map/rank_desc.js
@@ -1,6 +1,20 @@
 'use strict';
 var rank_desc = function(collection){
-  return collection.sort();
+    if(collection.length === 0 || collection.length === 1) {
+        return collection;
+    }
+
+    for(var i = 0; i < collection.length; i++) {
+        for(var j = i; j < collection.length; j++) {
+            if(collection[i] > collection[j]) {
+                collection[i] = collection[i] + collection[j];
+                collection[j] = collection[i] - collection[j];
+                collection[i] = collection[i] - collection[j];
+            }
+        }
+    }
+
+    return collection;
 };
 
 module.exports = rank_desc;
