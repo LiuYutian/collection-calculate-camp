1a094a50d2b4cf377a1c0ac3e0b94541ffd19622
LiuYutian <l709178159@gmail.com>
2015-05-29 07:46:26 +0800

    commit no lodash superposition_operation->collection_operation->letter_mapping_2.js

--- practices/superposition_operation/collection_operation/letter_mapping_2.js
+++ practices/superposition_operation/collection_operation/letter_mapping_2.js
@@ -1,9 +1,13 @@
 'use strict';
 
 function average_to_letter(collection) {
+    var sum = 0;
 
-  //在这里写入代码
+    for(var i = 0; i < collection.length; i++) {
+        sum += collection[i];
+    }
+
+    return String.fromCharCode(Math.ceil(sum/collection.length)+96);
 }
 
 module.exports = average_to_letter;
-
