63c16a59b49ca2a8a5d6f3757354005ff30cfa52
LiuYutian <l709178159@gmail.com>
2015-05-29 07:40:56 +0800

    commit no lodash superposition_operation->collection_operation->leter_mapping_1.js

--- practices/superposition_operation/collection_operation/letter_mapping_1.js
+++ practices/superposition_operation/collection_operation/letter_mapping_1.js
@@ -1,8 +1,15 @@
 'use strict';
 
 function even_to_letter(collection) {
+    var array = [];
 
-  //在这里写入代码
+    for(var i = 0; i < collection.length; i++) {
+        if(collection[i] % 2 === 0){
+            array[array.length] = String.fromCharCode(collection[i]+96);
+        }
+    }
+    
+    return array;
 }
 
 module.exports = even_to_letter;
