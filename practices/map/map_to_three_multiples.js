b8ef00f0da396c927b044cbec11e5dd4566d758f
LiuYutian <l709178159@gmail.com>
2015-05-29 14:45:48 +0800

    commit gao jian function map->map_to_three_multiples.js

--- practices/map/map_to_three_multiples.js
+++ practices/map/map_to_three_multiples.js
@@ -1,9 +1,17 @@
 'use strict';
 var map_to_three_multiples = function(collections){
+    return map(collections, even);
+}
+
+function even(value) {
+    return value * 3;
+}
+
+function map(collections, fun) {
     var array = [];
 
     for (var i = 0; i < collections.length; i++) {
-        array[array.length] = collections[i] * 3;
+        array.push(fun(collections[i]));
     }
 
     return array;
