e1768677c8b3d27945df9e21d44b45ad3092619d
LiuYutian <l709178159@gmail.com>
2015-05-29 09:17:06 +0800

    commit no lodash superposition_operation->interval_operation->median_to_letter.js

--- practices/superposition_operation/interval_operation/median_to_letter.js
+++ practices/superposition_operation/interval_operation/median_to_letter.js
@@ -1,8 +1,40 @@
 'use strict';
 
 function median_to_letter(collection) {
+    for(var i = 0; i < collection.length; i++) {
+        for(var j = i; j < collection.length; j++) {
+            if(collection[i] > collection[i]){
+                var temp = collection[i];
+                collection[i] = collection[j];
+                collection[j] = temp;
+            }
+        }
+    }
 
-  //在这里写入代码
+    if(collection % 2 === 0) {
+        return MapChar(parseInt((collection[collection.length/2]+collection[collection.length/2-1])/2));
+    }
+    return MapChar(parseInt(collection[parseInt(collection.length/2)]));
+
+}
+
+function MapChar(n) {
+    if(n>26) {
+        var carry = parseInt(n/26);
+        if(n%26 === 0) {
+            return String.fromCharCode(carry-1+96)+String.fromCharCode(26+96);
+        }
+        else {
+            return String.fromCharCode(carry+96)+String.fromCharCode(n%26+96);
+        }
+    }else {
+        if(n%26 === 0) {
+            return String.fromCharCode(26+96);
+        }
+        else {
+            return String.fromCharCode(n%26+96);
+        }
+    }
 }
 
 module.exports = median_to_letter;
