822fae87ea963a4816130d35074592fc86c4e7a4
LiuYutian <l709178159@gmail.com>
2015-05-29 12:29:22 +0800

    commit no lodash superposition_operation->own_elements_operation->subscript_is_even->calculate_median.js

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -1,5 +1,19 @@
 'use strict';
 var calculate_median = function(collection){
+    for(var i = 0; i < collection.length; i++) {
+        for(var j = i; j < collection.length; j++) {
+            if(collection[i] > collection[j]) {
+                var temp = collection[i];
+                collection[i] = collection[j];
+                collection[j] = temp;
+            }
+        }
+    }
 
+    if(collection.length % 2 === 0) {
+        return parseInt((collection[collection.length/2]+collection[collection.length/2-1])/2);
+    }else{
+        return collection[parseInt(collection.length/2)];
+    }
 };
 module.exports = calculate_median;
