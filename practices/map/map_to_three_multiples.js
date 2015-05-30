e5a8e746091ceca4e9dd9d5a487aeaac15b5aec5
LiuYutian <l709178159@gmail.com>
2015-05-30 23:18:29 +0800

    high tip map->map_to_three_mutiples.js

--- practices/map/map_to_three_multiples.js
+++ practices/map/map_to_three_multiples.js
@@ -1,20 +1,23 @@
 'use strict';
+
 var map_to_three_multiples = function(collections){
-    return map(collections, even);
+    return map(collections, executor);
 }
 
-function even(value) {
-    return value * 3;
+function executor(n) {
+    return n * 3;
 }
 
-function map(collections, fun) {
+function map(collections, executor){
     var array = [];
 
-    for (var i = 0; i < collections.length; i++) {
-        array.push(fun(collections[i]));
+    for(var i = 0; i < collections.length; i++) {
+        array.push(executor(collections[i]));
     }
 
     return array;
 }
+
+
 
 module.exports = map_to_three_multiples;
