abea8db92aa008361ce0792dd0e1f5f0225e8df3
LiuYutian <l709178159@gmail.com>
2015-05-31 19:16:59 +0800

    commit no lodash superpositioin_operation->own_elements_operation->subscript_is_even->even_group_calculate_average.js

--- practices/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
@@ -1,5 +1,39 @@
 'use strict';
 var even_group_calculate_average = function(collection){
+    var array = [];
+    var resultArray = [];
 
+    for(var i = 0; i < collection.length; i++) {
+        if(array[collection[i].toString().length-1] === undefined){
+            array[collection[i].toString().length-1] = new Array();
+        }
+        if(collection[i] % 2 === 0) {
+            array[collection[i].toString().length-1].push(collection[i]);
+        }
+    }
+
+    for(var i = 0; i < array.length; i++) {
+        var sum = 0;
+
+        if(array[i] === undefined){
+            continue;
+        }
+
+        for(var j = 0;j < array[i].length; j++) {
+            sum += array[i][j];
+        }
+
+        if(array[i].length != 0){
+            resultArray.push(sum/array[i].length);
+        }
+    }
+
+    for(var i = 0; i < resultArray.length; i++){
+        if(resultArray[i] != 0){
+            return resultArray;
+        }
+    }
+
+    return 0;
 };
 module.exports = even_group_calculate_average;
