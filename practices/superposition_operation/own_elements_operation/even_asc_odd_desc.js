31abcc41da8971b40b85c210447c604853778f64
LiuYutian <l709178159@gmail.com>
2015-05-31 20:32:38 +0800

    commit no loadsh superposition_operation->own_elements_operation->even_asc_odd_desc.js

--- practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,5 +1,27 @@
 'use strict';
 var even_asc_odd_desc = function(collection){
+    for(var i = 0; i < collection.length; i++) {
+        for(var j = i; j < collection.length; j++) {
+            if(collection[i] > collection[j]){
+                var temp = collection[i];
+                collection[i] = collection[j];
+                collection[j] = temp;
+            }
+        }
+    }
 
+    for(var i = 0; i < collection.length; i++) {
+        if(collection[i] % 2 === 0) {
+            evenArray.push(collection[i]);
+        }else {
+            oddArray.push(collection[i]);
+        }
+    }
+
+    for(var i = oddArray.length-1; i >= 0; i--) {
+        evenArray.push(oddArray[i]);
+    }
+
+    return evenArray;
 };
 module.exports = even_asc_odd_desc;
