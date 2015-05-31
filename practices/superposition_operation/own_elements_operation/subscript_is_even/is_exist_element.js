b6ecf1fe4d40babaaae3a28fab9ea397a5b9aff8
LiuYutian <l709178159@gmail.com>
2015-05-31 17:22:38 +0800

    commit no lodash superposition_operation->own_elements_operation->subscript_is_even->is_exist_element.js

--- practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,5 +1,11 @@
 'use strict';
 var is_exist_element = function(collection,element){
+    for(var i = 0; i < collection.length; i++) {
+        if(i % 2 === 0 && collection[i] === element) {
+            return true;
+        }
+    }
 
+    return false;
 };
 module.exports = is_exist_element;
