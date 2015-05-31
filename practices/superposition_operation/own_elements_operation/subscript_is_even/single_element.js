c20984066a01c0875ed4af8cb5390553e2ade531
LiuYutian <l709178159@gmail.com>
2015-05-31 19:47:13 +0800

    commit no lodash superposition_operation->own_elements_operation->subscript_is_even->single_element.js

--- practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,5 +1,40 @@
 'use strict';
 var single_element = function(collection){
+    var array = [];
 
+    for(var i = 1; i < collection.length; i++) {
+        if(i % 2 != 0) {
+            array.push(collection[i]);
+        }
+    }
+
+    var objectArray = [];
+
+    for(var i = 0; i < array.length; i++) {
+        var exist = false;
+        for(var j = 0; j < objectArray.length; j++) {
+            if(array[i] === objectArray[j].num) {
+                objectArray[j].count++;
+                exist = true;
+            }
+        }
+
+        if(!exist) {
+            objectArray.push({
+                num : array[i],
+                count : 1
+            })
+        }
+    }
+
+    var resultArray = [];
+
+    for(var i = 0; i < objectArray.length; i++) {
+        if(objectArray[i].count === 1){
+            resultArray.push(objectArray[i].num);
+        }
+    }
+
+    return resultArray;
 };
 module.exports = single_element;
