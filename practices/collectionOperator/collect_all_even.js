804d4e02ae83b7771803a21db6c9dcae07798f26
LiuYutian <l709178159@gmail.com>
2015-06-01 13:24:17 +0800

    change myLodash collectionOperator->collect_all_even.js

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -1,15 +1,13 @@
 'use strict';
 
 function collect_all_even(collection) {
-    var array = [];
+    var _ = require('../../myLodash.js');
 
-    for(var i = 0; i < collection.length; i++) {
-        if(collection[i]%2 === 0) {
-            array.push(collection[i]);
-        }
-    }
-
-    return array;
+    return _.filter(collection, function(n){
+        return n % 2 === 0;
+    });
 }
+
+var array = [];
 
 module.exports = collect_all_even;
