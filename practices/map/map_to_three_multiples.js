985eb72f2a3cc6f26278414d6c6266bc1961509d
LiuYutian <l709178159@gmail.com>
2015-06-01 12:58:58 +0800

    change myLodash map->map_to_three_multiples.js

--- practices/map/map_to_three_multiples.js
+++ practices/map/map_to_three_multiples.js
@@ -1,23 +1,11 @@
 'use strict';
 
+var _ = require("../../myLodash.js");
+
 var map_to_three_multiples = function(collections){
-    return map(collections, executor);
+    return _.map(collections, function executor(n){
+        return n * 3;
+    });
 }
-
-function executor(n) {
-    return n * 3;
-}
-
-function map(collections, executor){
-    var array = [];
-
-    for(var i = 0; i < collections.length; i++) {
-        array.push(executor(collections[i]));
-    }
-
-    return array;
-}
-
-
 
 module.exports = map_to_three_multiples;
