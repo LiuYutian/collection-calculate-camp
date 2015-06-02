5befcd1bfb634faff8f7c7c5855135027d1e1a7d
LiuYutian <l709178159@gmail.com>
2015-06-02 21:07:34 +0800

    change myLodash filter->choose_multiples_of_three.js

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -1,15 +1,11 @@
 'use strict';
 
 function choose_multiples_of_three(collection) {
-    var array = [];
+    var _ = require('../../myLodash.js');
 
-    for(var i = 0; i < collection.length; i++) {
-        if(collection[i] % 3=== 0) {
-            array.push(collection[i]);
-        }
-    }
-
-    return array;
+    return _.filter(collection, function(n){
+        return n % 3 === 0;
+    });
 }
 
 module.exports = choose_multiples_of_three;
