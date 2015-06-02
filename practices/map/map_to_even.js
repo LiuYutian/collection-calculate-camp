2d976af2c4cc2d0de0e7505fef1161ac75dc3bc2
LiuYutian <l709178159@gmail.com>
2015-06-02 21:14:53 +0800

    change myLodash map->map_to_even.js

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,12 +1,10 @@
 'use strict';
 function map_to_even(collection){
-    var array = [];
+    var _ = require('../../myLodash.js');
 
-    for(var i = 0; i < collection.length; i++) {
-        array[array.length] = collection[i] * 2;
-    }
-
-    return array;
+    return _.map(collection, function(n){
+        return n * 2;
+    })
 }
 
 module.exports = map_to_even;
