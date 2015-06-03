e7fe798c6ba87272e445f1af3c21da2bc13a2260
LiuYutian <l709178159@gmail.com>
2015-06-03 17:58:07 +0800

    change myLodash superposition_operation->interval_operation->amount_even.js

--- practices/superposition_operation/interval_operation/amount_even.js
+++ practices/superposition_operation/interval_operation/amount_even.js
@@ -1,6 +1,16 @@
 'use strict';
 
 function amount_even(collection) {
+    var _ = require('../../../myLodash.js');
+
+    var array = _.filter(collection, function(n) {
+        return n % 2 === 0;
+    });
+
+    return _.reduce(array, function(a, b){
+        return a+b;
+    });
+
     var sum = 0;
 
     for(var i = 0; i < collection.length; i++) {
