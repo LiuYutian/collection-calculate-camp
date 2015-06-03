1c8d437b0788882dc999b9d2a56dd338e57b5830
LiuYutian <l709178159@gmail.com>
2015-06-03 17:33:54 +0800

    change myLoadsh superposition_operation->collection_operation->practice_3.js

--- practices/superposition_operation/collection_operation/practice_3.js
+++ practices/superposition_operation/collection_operation/practice_3.js
@@ -14,17 +14,6 @@ function hybrid_operation_to_uneven(collection) {
     return _.reduce(mapArray, function(a, b){
         return a + b;
     });
-
-    var array = [];
-    var sum = 0;
-
-    for(var i = 0; i < collection.length; i++) {
-        if(collection[i] % 2 != 0) {
-            sum += collection[i] * 3 + 5;
-        }
-    }
-
-    return sum;
 }
 
 module.exports = hybrid_operation_to_uneven;
