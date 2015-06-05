3604043bad4bc0cedc96fcff1f7bbd6a47e5757a
LiuYutian <l709178159@gmail.com>
2015-06-05 14:00:37 +0800

    collectionOperator->get_integer_interval_2.js

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -1,44 +1,13 @@
 'use strict';
 
 function get_integer_interval_2(number_a, number_b) {
-    if(number_a < number_b) {
-        return aLessThanb(number_a, number_b);
-    }
+    var _ = require('../../myLodash.js');
 
-    if(number_a > number_b) {
-        return aGreaterb(number_a, number_b);
-    }
+    var array = _.range(number_a, number_b);
 
-    if(number_a % 2 === 0){
-        return [number_a];
-    }else{
-        return [];
-    }
-}
-
-
-function aLessThanb(number_a, number_b) {
-    var array = [];
-
-    for(var i = number_a; i <= number_b; i++) {
-        if(i%2 === 0){
-            array.push(i);
-        }
-    }
-
-    return array;
-}
-
-function aGreaterb(number_a, number_b) {
-    var array = [];
-
-    for(var i = number_a; i >= number_b; i--){
-        if(i%2 === 0){
-            array.push(i);
-        }
-    }
-
-    return array;
+    return _.filter(array, function(n){
+        return n % 2 === 0;
+    });
 }
 
 module.exports = get_integer_interval_2;
