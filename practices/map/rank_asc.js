b32d866596ed108a95dd903b1603844ce894e902
LiuYutian <l709178159@gmail.com>
2015-06-06 15:50:34 +0800

    change myLodash map->rank_asc.js

--- practices/map/rank_asc.js
+++ practices/map/rank_asc.js
@@ -1,19 +1,10 @@
 'use strict';
+
+var _ = require('../../myLodash.js');
+
 var rank_asc = function(collection){
-    if(collection.length === 0 || collection.length === 1) {
-        return collection;
-    }
-
-    for(var i = 0; i < collection.length; i++) {
-        for(var j = i; j < collection.length; j++) {
-            if(collection[i] < collection[j]) {
-                collection[i] = collection[i] + collection[j];
-                collection[j] = collection[i] - collection[j];
-                collection[i] = collection[i] - collection[j];
-            }
-        }
-    }
-
-    return collection;
+    return _.sort(collection, function(a, b){
+        return a < b;
+    });
 }
 module.exports = rank_asc;
