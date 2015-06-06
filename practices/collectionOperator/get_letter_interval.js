9407c98325ed7b6e82b7f84ef9af556eedccad6e
LiuYutian <l709178159@gmail.com>
2015-06-06 15:23:40 +0800

    change myLodash collectioinOperator->git_letter_interval.js

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -1,23 +1,16 @@
 'use strict';
 
+var _ = require('../../myLodash.js');
+
 function get_letter_interval(number_a, number_b) {
-    var array = [];
+    var array = _.range(number_a, number_b);
+    var result = [];
 
-    if(number_a < number_b) {
-        for(var i = number_a; i <= number_b; i++) {
-            array.push(String.fromCharCode(i+96));
-        }
-        return array;
-    }
+    _.each(array, function(n){
+        result.push(String.fromCharCode(n+96));
+    });
 
-    if(number_a > number_b) {
-        for(var i = number_a; i >= number_b; i--){
-            array.push(String.fromCharCode(i+96));
-        }
-        return array;
-    }
-
-    return [String.fromCharCode(number_a+96)];
+    return result;
 }
 
 module.exports = get_letter_interval;
