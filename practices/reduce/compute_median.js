68361425e7656a53d3b6e11c8a27b417ecfa65f4
LiuYutian <l709178159@gmail.com>
2015-06-06 16:11:12 +0800

    change reduce->compute_median.js

--- practices/reduce/compute_median.js
+++ practices/reduce/compute_median.js
@@ -1,21 +1,13 @@
 'use strict';
 
-function compute_median(collection) {
-    for(var i = 0; i < collection.length; i++) {
-        for(var j = i; j < collection.length; j++) {
-            if(collection[i] > collection[j]) {
-                collection[i] = collection[i] + collection[j];
-                collection[j] = collection[i] - collection[j];
-                collection[i] = collection[i] - collection[j];
-            }
-        }
-    }
+var _ = require('../../myLodash.js');
 
-    if(collection.length % 2 === 0) {
-        return (collection[collection.length/2-1]+collection[collection.length/2])/2;
-    }else{
-        return collection[parseInt(collection.length/2)];
-    }
+function compute_median(collection) {
+    collection = _.sort(collection, function(a, b){
+        return a > b;
+    });
+
+    return _.median(collection);
 }
 
 module.exports = compute_median;
