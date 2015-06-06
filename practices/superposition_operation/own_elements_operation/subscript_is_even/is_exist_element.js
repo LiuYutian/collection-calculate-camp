ccf4d615e105c95728b10d88984e2c1bb25096fb
LiuYutian <l709178159@gmail.com>
2015-06-06 18:07:45 +0800

    change myLodash superposition_operation->own_elements_operation->subscript_is_even->is_exist_element.js

--- practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,11 +1,12 @@
 'use strict';
-var is_exist_element = function(collection,element){
-    for(var i = 0; i < collection.length; i++) {
-        if(i % 2 === 0 && collection[i] === element) {
-            return true;
-        }
-    }
 
-    return false;
+var _ = require('../../../../myLodash.js');
+
+var is_exist_element = function(collection,element){
+    var filterArray = _.filter(collection, function(n, i){
+        return i % 2 === 0;
+    });
+
+    return _.exist(filterArray, element);
 };
 module.exports = is_exist_element;
