f8903acb31d2805090053556de416d0607c50b40
LiuYutian <l709178159@gmail.com>
2015-06-06 15:35:17 +0800

    change myLodash collectionOperator->get_union.js

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -1,30 +1,18 @@
 'use strict';
 
 function get_union(collection_a, collection_b) {
-    // var _  = require('../lodash/array');
-    //
-    // return _.union(collection_a, collection_b);
-
-    for(var i = 0; i < collection_b.length; i++) {
-        collection_a.push(collection_b[i]);
-    }
-
+    var _  = require('../../myLodash.js');
     var array = [];
 
-    for(var i = 0; i < collection_a.length; i++){
-        var exist = false;
-        for(var j = 0; j < array.length; j++) {
-            if(collection_a[i] == array[j]) {
-                exist = true;
-                break;
-            }
-        }
-        if(!exist) {
-            array.push(collection_a[i]);
-        }
-    }
+    _.each(collection_a, function(n){
+        array.push(n);
+    });
 
-    return array;
+    _.each(collection_b, function(n){
+        array.push(n);
+    });
+
+    return _.deWeight(array);
 }
 
 module.exports = get_union;
