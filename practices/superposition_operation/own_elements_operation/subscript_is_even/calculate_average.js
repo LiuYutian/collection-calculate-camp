6aed2e3d9c5dcd5ed4eff209375176aebcdf91fb
LiuYutian <l709178159@gmail.com>
2015-06-06 17:24:24 +0800

    superposition_operation->own_elements_operation->subscript_is_even->calculate_average.js

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,15 +1,19 @@
 'use strict';
+
+var _ = require('../../../../myLodash.js');
+
 var calculate_average = function(collection){
+    var array = _.filter(collection, function(n, i){
+        return i % 2 !== 0;
+    });
+
     var sum = 0;
-    var count = 0;
 
-    for(var i = 1; i < collection.length; i++) {
-        if(i % 2 === 0){
-            sum += collection[i];
-            count++;
-        }
-    }
+    _.each(array, function(n){
+        sum += n;
+    });
 
-    return sum/count;
+
+    return sum / array.length;
 };
 module.exports = calculate_average;
